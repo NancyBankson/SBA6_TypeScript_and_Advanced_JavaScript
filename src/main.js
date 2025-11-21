28a0e975b5949c0d3f02c9161ac83601863e47be
Nancy Bankson <nbankson3@gmail.com>
2025-11-21 14:37:19 -0600

    Product class completed

---
+++ src/main.js
@@ -0,0 +1,5 @@
+fetch('https://dummyjson.com/products/1')
+    .then(res => res.json())
+    .then(console.log);
+export {};
+//# sourceMappingURL=main.js.map
---
+++ src/models/Product.ts
@@ -0,0 +1,66 @@
+interface ProductDimensions {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+interface ProductMeta {
+  createdAt: string;
+  updatedAt: string;
+  barcode: string;
+  qrCode: string;
+}
+
+export class Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string;
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: ProductDimensions;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: string[];
+  returnPolicy: string;
+  meta: ProductMeta;
+  thumbnail: string;
+
+  constructor(id: number, title: string, description: string, category: string, price: number, discountPercentage: number, rating: number, stock: number, tags: string, brand: string, sku: string, weight: number, dimensions:  ProductDimensions, warrantyInformation: string, shippingInformation: string, availabilityStatus: string, reviews: string[], returnPolicy: string, meta: ProductMeta, thumbnail: string,) {
+  this.id = id;
+  this.title = title;
+  this.description = description;
+  this.category = category;
+  this.price = price;
+  this.discountPercentage = discountPercentage;
+  this.rating = rating;
+  this.stock = stock;
+  this.tags = tags;
+  this.brand = brand;
+  this.sku = sku;
+  this.weight = weight;
+  this.dimensions = dimensions;
+  this.warrantyInformation = warrantyInformation;
+  this.shippingInformation = shippingInformation;
+  this.availabilityStatus = availabilityStatus;
+  this.reviews = reviews;
+  this.returnPolicy = returnPolicy;
+  this.meta = meta;
+  this.thumbnail = thumbnail;
+  }
+
+  displayDetails(): string {
+return `${this.title} costs $${this.price} and is ${this.description}.`;
+}
+
+ getPriceWithDiscount() {
+        return Math.round(100*calculateDiscount(this, 0.1))/100;
+    }
+}
