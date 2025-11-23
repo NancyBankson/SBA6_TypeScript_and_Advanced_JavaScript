b9b2269b590cdc37c676fa054431ff54c0c95b2b
Nancy Bankson <nbankson3@gmail.com>
2025-11-23 15:23:36 -0600

    Fixed category display error

--- src/main.js
+++ src/main.js
@@ -5,11 +5,8 @@ import { fetchData } from "./services/apiService.js";
 fetchData()
     .then(data => {
     const productArray = data.products;
-    // for (let i=0; i<productArray!.length; i++) {
-    //   console.log(productArray[i]!.displayDetails);
-    // }
     for (let i = 0; i < productArray.length; i++) {
-        const newProduct = new Product(productArray[i].id, productArray[i].title, productArray[i].description, productArray[0].category, productArray[i].price, productArray[i].discountPercentage, productArray[i].rating);
+        const newProduct = new Product(productArray[i].id, productArray[i].title, productArray[i].description, productArray[i].category, productArray[i].price, productArray[i].discountPercentage, productArray[i].rating);
         console.log(newProduct.displayDetails());
         console.log(`
       Total price with tax and discount: $ ${newProduct.getPriceWithDiscount()}`);
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Product }  from "./models/Product.js"
+import { Product } from "./models/Product.js"
 import { calculateTax } from "./utils/taxCalculator.js"
 import { calculateDiscount } from "./utils/discountCalculator.js"
 import { fetchData } from "./services/apiService.js"
@@ -7,14 +7,14 @@ import { fetchData } from "./services/apiService.js"
 fetchData()
   .then(data => {
     const productArray: Product[] = data!.products;
-    for (let i=0; i<productArray!.length; i++) {
-       const newProduct = new Product(productArray[i]!.id, productArray[i]!.title, productArray[i]!.description, productArray[0]!.category, productArray[i]!.price, productArray[i]!.discountPercentage, productArray[i]!.rating);
-    console.log(newProduct.displayDetails());
-    console.log(`
+    for (let i = 0; i < productArray!.length; i++) {
+      const newProduct = new Product(productArray[i]!.id, productArray[i]!.title, productArray[i]!.description, productArray[i]!.category, productArray[i]!.price, productArray[i]!.discountPercentage, productArray[i]!.rating);
+      console.log(newProduct.displayDetails());
+      console.log(`
       Total price with tax and discount: $ ${newProduct.getPriceWithDiscount()}`)
     }
 
-   
+
   })
   .catch(error => console.error("Fetch error:", error));
 
